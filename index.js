2a4b83bba6547c37c48b43570f00a5e72e30a7c8
Marie-Eve Gauthier <gauthier.marie.eve@gmail.com>
2016-05-02 21:12:19 +0000

    exercise 3 completed

--- index.js
+++ index.js
@@ -14,12 +14,73 @@ app.get('/hello', function (req, res) {
 });
 
 
-/* This is a web server that can listen to requests for /hello?name=firstName, 
+/* This is a web server that can listen to requests for /hello/name, 
 and respond with some HTML that says <h1>Hello _name_!</h1>. 
 */
 app.get('/hello/:name', function(req, res) {
   res.send('<h1>Hello ' + req.params.name + '!<h1>');
 });
+
+/*This is a web server that can listen to requests for /calculator/:operation?num1=XX&num2=XX 
+and respond with a JSON object that looks like the following. For example, /op/add?num1=31&num2=11:
+
+{
+  "operator": "add",
+  "firstOperand": 31,
+  "secondOperand": 11,
+  "solution": 42
+}
+*/
+
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+function sub(num1, num2) {
+  return num1 - num2;
+}
+
+function mult(num1, num2) {
+  return num1 * num2;
+}
+
+function div(num1, num2) {
+  return num1 / num2;
+}
+
+app.get('/calculator/:operation/:num1/:num2', function(req, res) {
+    var op  = req.params.operation;
+    var leresult;
+      switch (op) {
+        case 'add':
+         leresult = add(Number(req.params.num1), Number(req.params.num2));
+         break;
+        case 'sub':
+          leresult = sub(Number(req.params.num1), Number(req.params.num2));
+          break;
+        case 'mult':
+          leresult = mult(Number(req.params.num1), Number(req.params.num2));
+          break;
+        case 'div':  
+          leresult = div(Number(req.params.num1), Number(req.params.num2));
+          break;
+        default:
+          leresult = "error";
+      }
+      if((leresult === 'error') || (isNaN(Number(req.params.num1)) || isNaN(Number(req.params.num2)))) {
+        res.send('This is not a valid operation or number');
+      }
+      else {
+        var finalResult = {
+          operator: req.params.operation,
+          firstOperand: req.params.num1,
+          secondOperand: req.params.num2,
+          solution: leresult 
+        };
+        res.send(finalResult);
+      }
+});
+
 
 /* YOU DON'T HAVE TO CHANGE ANYTHING BELOW THIS LINE :) */
 
